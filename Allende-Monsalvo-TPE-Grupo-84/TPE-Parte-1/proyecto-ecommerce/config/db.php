413ca1fd9c721f7c6f2fe58f73e546e5cac7e829
sntxDev <sntxdev@gmail.com>
2023-10-07 15:23:50 -0300

    se intregra la carpeta proyecto-ecommerce

---
+++ Allende-Monsalvo-TPE-Grupo-84/TPE-Parte-1/proyecto-ecommerce/config/db.php
@@ -0,0 +1,16 @@
+<?php
+// Solo conexión a base de datos
+
+/*   
+    EJEMPLO PARA ENTENDER MEJOR LA FUNCIONALIDAD DEL ARCHIVO
+
+    Acá iria 
+    function getConnection(){
+        devuelve una variable $db que va a tomar un PDO(donde esté localizado la tabla de juegos)
+        $db= new 
+        PDO('mysql:host=localhost;dbname=db_ecommerce;charset=utf8','root', '');
+        
+        return $db;
+    
+    }
+*/
