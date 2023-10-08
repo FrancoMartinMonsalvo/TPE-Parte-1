eaffbb9396bc1ad0b561efddd0ebc25b474c1604
unknown <ass_fran@hotmail.com>
2023-10-08 02:50:51 -0300

    ultimos conflictos solucionados (creo)

--- Allende-Monsalvo-TPE-Grupo-84/TPE-Parte-1/proyecto-ecommerce/router.php
+++ Allende-Monsalvo-TPE-Grupo-84/TPE-Parte-1/proyecto-ecommerce/router.php
@@ -11,41 +11,46 @@ if (!empty($_GET['action'])) {
     $action = $_GET['action'];
 }
 
-// listar Juegos   -->   showGames();
-// agregar Juegos-> addGame();
-// eliminar Juegos/ :ID -> removeGame($id);
-
-
-
-// parsea la accion para separar accion real de parametros
-//los metodos que llama son los de functionalities php
 $params = explode('/', $action);
-$controllerGames = new GameController();
-$controllerCategories = new CategoryController();
 $controllerGames = new GameController();
 $controllerCategories = new CategoryController();
 
 switch ($params[0]){
     case 'home':
-        $controllerGames->showGames();
-        break;
+            $controllerGames->showGames();
+        
+            break;
     case 'categories':
-        $controllerCategories->showCategories();
-        break;
+            $controllerCategories->showCategories();
+        
+            break;
     case 'category':
-        $controllerCategories->showCategoryById($params[1]);
-        break;
+            $controllerCategories->showCategoryById($params[1]);
+            
+            break;
     case 'register':
-        $controller = new UserController();
-        $controller->signup(); 
-        break;
-    case 'category':
-        $controllerCategories->showCategoryById($params[1]);
-        break;
-    case 'register':
-        $controller = new UserController();
-        $controller->signup(); 
-        break;
+            $controller = new UserController();
+            $controller->signup(); 
+            
+            break;
+    case 'signup':
+                $controller = new UserController();
+                $controller->showSignupForm();  
+            
+            break;         
+    case 'login':
+            $controller = new UserController();
+            $controller->showLogin(); 
+            
+            break;
+    case 'auth':
+            $controller = new AuthController();
+            $controller->auth();
+            
+            break;
+    case 'logout':
+            $controller = new UserController();
+            $controller->logout();
     default:
         echo "404 Page Not Found";
         break;
